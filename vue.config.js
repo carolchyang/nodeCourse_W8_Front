4c5592d1e99b8855c5c23fbb85f58bb7c0d343c8
carolchyang <chingheui.yang@gmail.com>
2022-06-12 15:39:04 +0800

    修改路徑

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
-    process.env.NODE_ENV === "production" ? "/nodeCourse_W7_Front/" : "/",
+    process.env.NODE_ENV === "production" ? "/nodeCourse_W8_Front/" : "/",
   configureWebpack: {
     performance: {
       hints: "warning",
